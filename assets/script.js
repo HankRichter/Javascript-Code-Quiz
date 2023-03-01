1ae492201bbd319161e6f79dd46c1292a103e91e
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-03-01 17:42:47 -0500

    setInterval function
    This function is within the timerScore function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
 // 1.timer starts and counts down when start is clicked
+function timerScore() {
+  let timeLeft = 75;
+
+  const countdown = setInterval(function () {
+    timeLeft--;
+    timer.innerHTML = timeLeft + " score";
+
+    if (timeLeft === 0) {
+      clearInterval(countdown);
+    }
+  }, 1000);
+}
 // 2.first question appears when start is clicked
 // 3.capture the answer of the first question
 // 4.rinse and repeat for the questions and capturing answers
