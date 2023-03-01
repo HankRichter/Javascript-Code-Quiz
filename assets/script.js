7c284d8cea18b562fcbc25d8392577e04b890dc0
Hank Richter <hankrichter97@gmail.com>
2023-03-01 15:51:41 -0500

    added comments to work through the JS file

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// 1.timer starts and counts down when start is clicked
+// 2.first question appears when start is clicked
+// 3.capture the answer of the first question
+// 4.rinse and repeat for the questions and capturing answers
+// 5.score appears when last quesiton is answered
+// 6.allow user to end intials for score
+// 7. highscore table is viewable
