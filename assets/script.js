fa181ad400865999640a47fecbb9c5a7546b6735
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-03-01 17:44:13 -0500

    Editing the comments for tasks

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,8 @@ function timerScore() {
 // 2.first question appears when start is clicked
 // 3.capture the answer of the first question
 // 4.rinse and repeat for the questions and capturing answers
-// 5.score appears when last quesiton is answered
-// 6.allow user to end intials for score
-// 7. highscore table is viewable
+// 6.final score appears when last quesiton is answered
+// 7.allow user to end intials for score
+// 8. highscore table is viewable
+
+startButton.addEventListener("click", timerScore);
